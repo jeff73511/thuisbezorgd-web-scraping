3921d293cc256b6d5b67307f866950d8fa5fe01b
Jeff <jeff73511@msn.com>
2020-04-09 20:29:21 +0200

    update checkdirectory

--- thuisbezorgd.py
+++ thuisbezorgd.py
@@ -1,6 +1,6 @@
+import sqlite3
 import sys
 import time
-import json
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -10,10 +10,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait as wait
 
+
 db = "thuisbezorgd.db"
 if os.path.isfile(db):
     os.remove(db)
-
 
 website = "https://www.thuisbezorgd.nl/en/"
 driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -31,66 +31,92 @@ search_bar.send_keys(Keys.ENTER)
 time.sleep(2)
 driver.find_element_by_xpath("/html/body/div[5]/section/article/button").click()
 
-wait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Show more']"))).click()
-wait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Dutch']"))).click()
-
-
-driver.find_element_by_xpath("/html/body/div[8]/div/button").click()
-
-
-# (1) choose a cuisine
-
-# (2) if no
-pop_up_content = driver.find_element_by_xpath("/html/body/div[8]/div/div[2]")
-driver.execute_script("arguments[0].scrollTop = 200", pop_up_content)
-
-
-
-# # (3) if no
-# driver.execute_script("arguments[0].scrollTop = 400", scroll_down)
-
-# (4) if no, close the windown
-driver.find_element_by_xpath("/html/body/div[8]/div/button").click()
-
-
 time.sleep(5)
 plain_text = driver.page_source
 
-with open("cuisine_dic.json") as json_file:
-    cuisine_dic = json.load(json_file)
-
+record = []
 while True:
-    cuisine = input("Enter a cuisine style (or 'last page'/'next page'/'exit'):")
-    while True:
-        try:
-            if cuisine == "next page":
-                driver.find_element_by_xpath(
-                    "//*[@id='kitchen-types']/div/div/div[3]"
-                ).click()
 
-            elif cuisine == "last page":
-                driver.find_element_by_xpath(
-                    "//*[@id='kitchen-types']/div/div/div[1]"
-                ).click()
+    try:
+        cuisine = input(
+            "Choose a cuisine style or, enter 'All', 'Show more', or 'exit':"
+        )
 
-            elif cuisine == "exit":
-                break
-
+        if cuisine not in ["Show more", "exit"]:
+            if cuisine in record:
+                print(f"You already hit {cuisine}! Try something else!")
+                continue
             else:
-                driver.find_element_by_xpath(
-                    f"//*[@id='kitchen-types']/div/div/div[2]/div/a[{cuisine_dic[cuisine]}]"
+                wait(driver, 2).until(
+                    EC.element_to_be_clickable(
+                        (
+                            By.XPATH,
+                            f"//span[@class='swiper-slide__context'][text()='{cuisine}']",
+                        )
+                    )
                 ).click()
-
                 restaurants(cuisine=cuisine, html=plain_text)
+                record.append(cuisine)
 
+        elif cuisine == "Show more":
+            wait(driver, 2).until(
+                EC.element_to_be_clickable(
+                    (
+                        By.XPATH,
+                        f"//span[@class='swiper-slide__context'][text()='{cuisine}']",
+                    )
+                )
+            ).click()
+            pop_up = driver.find_element_by_xpath("/html/body/div[8]/div/div[2]")
+            driver.execute_script(f"arguments[0].scrollTop = 0", pop_up)
+
+            i = 0
+            while True:
+                i = i + 1
+                height = 200 * ((i % 3))
+                scroll = "scroll down"
+                if height == 0:
+                    scroll = "scroll up"
+
+                try:
+                    cuisine = input(
+                        f"Choose a cuisine style, or enter '{scroll}' or 'x':"
+                    )
+                    if cuisine == "x":
+                        driver.find_element_by_xpath(
+                            "/html/body/div[8]/div/button"
+                        ).click()
+                        break
+
+                    elif cuisine == scroll:
+                        driver.execute_script(
+                            f"arguments[0].scrollTop = {height}", pop_up
+                        )
+                        continue
+
+                    else:
+                        if cuisine in record:
+                            print(f"You already hit {cuisine}! Try something else!")
+                            i = i - 1
+                            continue
+                        wait(driver, 2).until(
+                            EC.element_to_be_clickable(
+                                (
+                                    By.XPATH,
+                                    f"//span[@class='tv-chip__inner-content'][text()='{cuisine}']",
+                                )
+                            )
+                        ).click()
+                        restaurants(cuisine=cuisine, html=plain_text)
+                        record.append(cuisine)
+                        break
+                except:
+                    i = i - 1
+                    print("Incorrect input! Try again!")
+
+        else:
+            driver.quit()
             break
 
-        except:
-            print("Input is not correct! Try again.")
-            cuisine = input(
-                "Enter a cuisine style (or 'last page'/'next page'/'exit'):"
-            )
-
-    if cuisine == "exit":
-        driver.quit()
-        sys.exit()
+    except:
+        print("Incorrect input! Try again!")
